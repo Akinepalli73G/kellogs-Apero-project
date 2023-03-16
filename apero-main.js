3d2d3fd77376eec862ea8235ea9656c9c015ceb9
Santhosh Akinepalli <santhosh.akinepalli@UG-GW-MAC118.local>
2023-03-16 17:30:14 +0530

    implemnting clicks to burger menu style

---
+++ apero-main.js
@@ -0,0 +1,15 @@
+opened = false;
+closed = false;
+window.onload = function() {
+  var clickBurgerMenuClose = document.querySelector('#mobile-burger-menu');
+  var clickBurgerMenuOpen = document.querySelector('#burger-close-button');
+  clickBurgerMenuClose.addEventListener('click', onBtnClick)
+  clickBurgerMenuOpen.addEventListener('click', onBtnClick)
+}
+
+function onBtnClick(e) {
+  var burgerMenuClose = document.querySelector('#mobile-burger-menu-close');
+  var burgerMenuOpen = document.querySelector('#mobile-burger-menu-open');
+  burgerMenuClose.classList.toggle('opened');
+  burgerMenuOpen.classList.toggle('closed');
+}
