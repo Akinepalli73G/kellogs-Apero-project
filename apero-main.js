492bbdf1a94d28292c1d7aa32a35445faffc4e9f
Santhosh Akinepalli <santhosh.akinepalli@UG-GW-MAC118.local>
2023-03-21 22:01:57 +0530

    Implementing already page template

--- apero-main.js
+++ apero-main.js
@@ -1,11 +1,15 @@
 opened = false;
 closed = false;
+languageOpened = false
 window.onload = function() {
   var clickBurgerMenuClose = document.querySelector('#mobile-burger-menu');
   var clickBurgerMenuOpen = document.querySelector('#burger-close-button');
-  clickBurgerMenuClose.addEventListener('click', onBtnClick)
-  clickBurgerMenuOpen.addEventListener('click', onBtnClick)
+  var clickLanguageNavigation = document.querySelector('.languagenavigation nav ul');
+  clickBurgerMenuClose.addEventListener('click', onBtnClick);
+  clickBurgerMenuOpen.addEventListener('click', onBtnClick);
+  clickLanguageNavigation.addEventListener('click', onLanguageDropdownClick);
 }
+
 
 function onBtnClick(e) {
   var burgerMenuClose = document.querySelector('#mobile-burger-menu-close');
@@ -14,4 +18,8 @@ function onBtnClick(e) {
   burgerMenuOpen.classList.toggle('closed');
   document.querySelector('#competition-landing-main-content-container').classList.toggle('closed');
   document.querySelector('#apero-main-content-container').classList.toggle('closed');
+}
+
+function onLanguageDropdownClick (e) {
+  document.querySelector('.languagenavigation nav')[0].classList.toggle('languageOpened');
 }
