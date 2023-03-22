e324c725487d7f70e5731a8a9b02e165a9be48f2
Santhosh Akinepalli <santhosh.akinepalli@UG-GW-MAC118.local>
2023-03-22 18:28:01 +0530

    Implementing UI fixes

--- apero-main.js
+++ apero-main.js
@@ -1,6 +1,7 @@
 opened = false;
 closed = false;
 languageOpened = false;
+mobileUpArrow = false;
 const  navLanguageContainer = document.querySelector('.languagenavigation nav');
 const downArrowClickButton = document.createElement("button");
 downArrowClickButton.classList.add('mobile-down-arrow');
@@ -24,4 +25,6 @@ function onBtnClick(e) {
 var clickLanguageNavigation = document.querySelector('.languagenavigation nav button');
 clickLanguageNavigation.addEventListener('click', () => {
   document.querySelector('.languagenavigation nav ul').classList.toggle('languageOpened');
+  document.querySelector('.mobile-down-arrow').classList.toggle('mobileUpArrow');
+  mobile-down-arrow
 });
