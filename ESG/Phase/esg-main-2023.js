b16b95dcb41ea470892648de9cdcb510c30f106e
Santhosh Akinepalli <santhosh.akinepalli@UG-GW-MAC118.local>
2023-05-24 21:29:54 +0530

    Implementing Header and footer

--- ESG/Phase/esg-main-2023.js
+++ ESG/Phase/esg-main-2023.js
@@ -1,107 +1,105 @@
-
-  // Crousel component start
-  var carouselContainer = document.querySelector(
-    ".esg-carousel-container .cmp-container"
-  );
-  if (carouselContainer) {
-    tns({
-      container: carouselContainer,
-      responsive: {
-        768: {
-          items: 1,
-        },
-        1990: {
-          items: 5,
-        },
+// Crousel component start
+var carouselContainer = document.querySelector(
+  ".esg-carousel-container .cmp-container"
+);
+if (carouselContainer) {
+  tns({
+    container: carouselContainer,
+    responsive: {
+      768: {
+        items: 1,
       },
-      gutter: 62,
-      fixedWidth: 100,
-      mouseDrag: true,
-      swipeAngle: false,
-      speed: 400,
-    });
+      1990: {
+        items: 5,
+      },
+    },
+    gutter: 62,
+    fixedWidth: 100,
+    mouseDrag: true,
+    swipeAngle: false,
+    speed: 400,
+  });
+}
+
+//carousel end
+
+// language seletor start
+
+languageOpened = false;
+mobileUpArrow = false;
+selectedLanguage = false;
+const navLanguageContainer = document.querySelectorAll(
+  ".languagenavigation nav li"
+);
+navLanguageContainer.forEach((navLanguage) => {
+  if (navLanguage) {
+    const downArrowClickButton = document.createElement("button");
+    downArrowClickButton.classList.add("mobile-down-arrow");
+    navLanguage.appendChild(downArrowClickButton)
   }
+});
 
-  //carousel end
+const desktopLanguagenavigationClicked = document.querySelectorAll(
+  ".languagenavigation #desktop-language-navigation button"
+);
+const mobileLanguagenavigationClicked = document.querySelectorAll(
+  ".languagenavigation #mobile-language-navigation button"
+);
 
+desktopLanguagenavigationClicked[0].addEventListener("click", () => {
+  document.querySelector(".languagenavigation nav ul").classList.toggle("languageOpened");
+  desktopLanguagenavigationClicked[0].classList.toggle("mobileUpArrow");
+  document.querySelector("body").classList.toggle("languageOpened");
+  document.querySelector("#esg-main-container").classList.toggle("languageOpened");
+});
 
-// // language seletor start
+mobileLanguagenavigationClicked[0].addEventListener("click", () => {
+  document.querySelectorAll(".languagenavigation nav ul")[1].classList.toggle("languageOpened");
+  mobileLanguagenavigationClicked[0].classList.toggle("mobileUpArrow");
+  document.querySelector("body").classList.toggle("languageOpened");
+  document.querySelector("#esg-main-container").classList.toggle("languageOpened");
+});
 
-// languageOpened = false;
-// mobileUpArrow = false;
-// selectedLanguage = false;
-// const navLanguageContainer = document.querySelectorAll(
-//   ".languagenavigation nav li"
-// );
-// navLanguageContainer.forEach((navLanguage) => {
-//   if (navLanguage) {
-//     const downArrowClickButton = document.createElement("button");
-//     downArrowClickButton.classList.add("mobile-down-arrow");
-//     navLanguage.appendChild(downArrowClickButton)
-//   }
-// });
+if (document.querySelectorAll(".languagenavigation")) {
+  Array.from(
+    document.querySelectorAll(".languagenavigation nav ul li")
+  ).forEach((selectedLangugae) => {
+    if (window.location.href == selectedLangugae.querySelector("a").href) {
+      selectedLangugae.classList.toggle("selectedLanguage");
+    }
+  });
+}
 
-// const desktopLanguagenavigationClicked = document.querySelectorAll(
-//   ".languagenavigation #desktop-language-navigation button"
-// );
-// const mobileLanguagenavigationClicked = document.querySelectorAll(
-//   ".languagenavigation #mobile-language-navigation button"
-// );
+// language seletor end
 
-// desktopLanguagenavigationClicked[0].addEventListener("click", () => {
-//   document.querySelector(".languagenavigation nav ul").classList.toggle("languageOpened");
-//   desktopLanguagenavigationClicked[0].classList.toggle("mobileUpArrow");
-//   document.querySelector("body").classList.toggle("languageOpened");
-//   document.querySelector("#esg-main-container").classList.toggle("languageOpened");
-// });
+// clcik on burger menu open start
+burgerMenuOpened = false;
+burgerMenuClosed = false;
 
-// mobileLanguagenavigationClicked[0].addEventListener("click", () => {
-//   document.querySelectorAll(".languagenavigation nav ul")[1].classList.toggle("languageOpened");
-//   mobileLanguagenavigationClicked[0].classList.toggle("mobileUpArrow");
-//   document.querySelector("body").classList.toggle("languageOpened");
-//   document.querySelector("#esg-main-container").classList.toggle("languageOpened");
-// });
+let onBtnClick = () => {
+  burgerMenuButton = document.querySelector(
+    ".button .cmp-button__icon--esg-burger-icon"
+  );
+  burgerMenuButton.classList.toggle("burgerMenuOpened");
+  langauageNavigationHide = document.querySelectorAll(
+    "#burger-menu .languagenavigation"
+  );
+  langauageNavigationHide[0].classList.toggle("burgerMenuClosed");
+  burgerMenuClose = document.querySelectorAll("#burger-close-menu");
+  burgerMenuClose[0].classList.toggle("burgerMenuOpened");
+  document
+    .querySelectorAll("#esg-main-container")[0]
+    .classList.toggle("burgerMenuOpened");
+  document
+    .querySelectorAll("#esg-footer-container")[0]
+    .classList.toggle("burgerMenuOpened");
+};
 
-// if (document.querySelectorAll(".languagenavigation")) {
-//   Array.from(
-//     document.querySelectorAll(".languagenavigation nav ul li")
-//   ).forEach((selectedLangugae) => {
-//     if (window.location.href == selectedLangugae.querySelector("a").href) {
-//       selectedLangugae.classList.toggle("selectedLanguage");
-//     }
-//   });
-// }
+document
+  .querySelector(".button .cmp-button__icon--esg-burger-icon")
+  .addEventListener("click", onBtnClick);
+document
+  .querySelector(".button .cmp-button__icon--burger-close-icon")
+  .addEventListener("click", onBtnClick);
 
-// // language seletor end
-
-// // clcik on burger menu open start
-// burgerMenuOpened = false;
-// burgerMenuClosed = false;
-
-// let onBtnClick = () => {
-//   burgerMenuButton = document.querySelector(
-//     ".button .cmp-button__icon--esg-burger-icon"
-//   );
-//   burgerMenuButton.classList.toggle("burgerMenuOpened");
-//   langauageNavigationHide = document.querySelectorAll(
-//     "#burger-menu .languagenavigation"
-//   );
-//   langauageNavigationHide[0].classList.toggle("burgerMenuClosed");
-//   burgerMenuClose = document.querySelectorAll("#burger-close-menu");
-//   burgerMenuClose[0].classList.toggle("burgerMenuOpened");
-//   document
-//     .querySelectorAll("#esg-main-container")[0]
-//     .classList.toggle("burgerMenuOpened");
-//   document
-//     .querySelectorAll("#esg-footer-container")[0]
-//     .classList.toggle("burgerMenuOpened");
-// };
-
-// document
-//   .querySelector(".button .cmp-button__icon--esg-burger-icon")
-//   .addEventListener("click", onBtnClick);
-// document
-//   .querySelector(".button .cmp-button__icon--burger-close-icon")
-//   .addEventListener("click", onBtnClick);
-
-// // clcik on burger menu close
+// clcik on burger menu close
